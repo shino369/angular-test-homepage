3bfe75247a3a06274bd827b5db32dbcaa7acecc4
Anthony Wong <nakagame02@gmail.com>
2022-05-19 18:27:00 +0800

    update prettier

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,15 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
+import { RouterModule } from '@angular/router'
 
 // UI librairies
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LayoutModule } from '@angular/cdk/layout';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { LayoutModule } from '@angular/cdk/layout'
+import { MatCommonModule } from '@angular/material/core'
 
-const COMMON = [    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    RouterModule,]
-const UIMODULES = [NgbModule, LayoutModule]
+const COMMON = [CommonModule, FormsModule, ReactiveFormsModule, RouterModule]
+const UIMODULES = [NgbModule, LayoutModule, MatCommonModule]
 
 // remove never later
 const COMPONENTS: never[] = []
@@ -20,16 +18,8 @@ const PIPES: never[] = []
 const DECLARE = [...COMPONENTS, ...DIRECTIVES, ...PIPES]
 @NgModule({
   declarations: DECLARE,
-  imports: [
-    ...COMMON,
-    ...UIMODULES,
-  ],
+  imports: [...COMMON, ...UIMODULES],
   providers: [],
-  exports: [
-    ...COMMON,
-    ...COMPONENTS,
-    ...UIMODULES,
-    ...PIPES,
-  ],
+  exports: [...COMMON, ...COMPONENTS, ...UIMODULES, ...PIPES],
 })
-export class SharedModule { }
+export class SharedModule {}
