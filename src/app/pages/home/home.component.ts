b072c52ed571afb8d9310942cce18fb59e28be1f
Anthony Wong <nakagame02@gmail.com>
2022-05-22 04:11:34 +0900

    update: copy from main repo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,49 +1,183 @@
-import { Component, HostListener, OnInit } from '@angular/core'
+import {
+  ChangeDetectorRef,
+  Component,
+  HostListener,
+  OnInit,
+} from '@angular/core'
 import { gsap } from 'gsap'
-import { ScrollTrigger } from 'gsap/all'
+import { ScrollTrigger, TextPlugin } from 'gsap/all'
 import * as _ from 'lodash'
 import { round } from 'lodash'
+import { trigger, state, transition, style, animate } from '@angular/animations'
+import { Subscription, BehaviorSubject, fromEvent } from 'rxjs'
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators'
+import { Title, Meta } from '@angular/platform-browser'
 
+import {
+  Breakpoint,
+  BreakpointsService,
+} from '@app/shared/services/breakpoints.service'
 @Component({
+  selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('visibilityChanged', [
+      state(
+        'expand',
+        style({
+          opacity: 1,
+
+          transform: 'translateX(0)',
+        })
+      ),
+      state(
+        'none',
+        style({
+          opacity: 0,
+
+          overflow: 'hidden',
+          transform: `translateX(-768px)`,
+        })
+      ),
+      transition('expand => none', animate('300ms')),
+      transition('none => expand', animate('300ms')),
+    ]),
+    trigger('loading', [
+      state(
+        'true',
+        style({
+          opacity: 1,
+        })
+      ),
+      state(
+        'false',
+        style({
+          opacity: 0,
+          display: 'none',
+        })
+      ),
+      transition('true => false', [animate('300ms')]),
+    ]),
+  ],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  OS = window.navigator.platform
+  disableFullpage: boolean = false
+  loading: string = 'true'
+  breakpoints: Breakpoint
+  filterHidden: boolean = true
+  breakpointSubscription: Subscription
+  visiblityState$ = new BehaviorSubject<string>('none')
+  backdropSub: Subscription
+  scrollEvent: Subscription
 
-  ngOnInit(): void {}
+  constructor(
+    private breakpointsService: BreakpointsService,
+    private metaService: Meta,
+    private cdref: ChangeDetectorRef
+  ) {
+    this.metaService.addTag({
+      property: 'og:image',
+      content:
+        'https://i.pinimg.com/originals/c9/72/f0/c972f0909879d3ce4137c7140e26922c.jpg',
+    })
+  }
+
+  ngOnInit(): void {
+    if (
+      ['iPad Simulator', 'iPhone Simulator', 'iPad', 'iPhone'].includes(
+        navigator.platform
+      ) ||
+      (navigator.userAgent.includes('Mac') && 'ontouchend' in document)
+    ) {
+      this.disableFullpage = true
+      console.log('disable fullpage')
+    }
+
+    this.breakpointSubscription =
+      this.breakpointsService.breakpointsSubject.subscribe(
+        (breakpoints: Breakpoint) => {
+          this.breakpoints = breakpoints
+          console.log(this.breakpoints)
+        }
+      )
+
+    this.scrollEvent = fromEvent(window, 'scroll')
+      .pipe(debounceTime(50))
+      .subscribe(() => {
+        if (this.loading === 'false') {
+          this.activeSlide = round(window.scrollY / window.innerHeight)
+          if (!this.disableFullpage) {
+            this.scroll(this.keyArr[this.activeSlide])
+          }
+        }
+      })
+    this.subBackdrop()
+  }
 
   ngAfterViewInit(): void {
-    gsap.registerPlugin(ScrollTrigger)
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class.
+    gsap.registerPlugin(ScrollTrigger, TextPlugin)
+    if (this.loading === 'true') {
+      this.nameEffect()
+    }
     this.parallax()
+    this.textEffect()
+    //scroll to top
+
     // this.horizontal()
-    console.log(this.keyArr)
+    // console.log(this.keyArr)
+    setTimeout(() => {
+      this.cursor.pause()
+      this.loading = 'false'
+      window.scrollTo(0, 0)
+      this.activeSlide = round(window.scrollY / window.innerHeight)
+      this.quoteEffect()
+    }, 12000)
+  }
+
+  ngAfterViewChecked(): void {
+    //Called after every check of the component's view. Applies to components only.
+    //Add 'implements AfterViewChecked' to the class.
+    this.cdref.detectChanges()
+  }
+
+  ngOnDestroy(): void {
+    //Called once, before the instance is destroyed.
+    //Add 'implements OnDestroy' to the class.
+    if (this.backdropSub) this.backdropSub.unsubscribe()
+    if (this.breakpointSubscription) this.breakpointSubscription.unsubscribe()
+    if (this.scrollEvent) this.scrollEvent.unsubscribe()
   }
 
   keyArr: any = []
   activeSlide: number = 0
 
-  // horizontal() {
-  //   let sections = gsap.utils.toArray('.panel')
-
-  //   sections.forEach((section: any, i) => {
-  //     section.bg = section.querySelector('.bg')
-  //     section.bg.style.backgroundImage = `url(https://picsum.photos/${1280}/${720}?random=${i}&blur=${3})`
-  //   })
-
-  //   gsap.to(sections, {
-  //     xPercent: -100 * (sections.length - 1),
-  //     ease: 'none',
-  //     scrollTrigger: {
-  //       trigger: '.horizontal',
-  //       pin: true,
-  //       scrub: 1,
-  //       snap: 1 / (sections.length - 1),
-  //       // base vertical scrolling on how wide the container is so it feels more natural.
-  //       end: '+=3500',
-  //     },
-  //   })
-  // }
+  subBackdrop() {
+    this.backdropSub = this.visiblityState$.subscribe(state => {
+      // console.log(state)
+      if (state === 'expand') {
+        document.body.style.overflow = 'hidden'
+      } else {
+        document.body.style.overflow = 'auto'
+      }
+    })
+  }
+  clickHidden(goback?: boolean) {
+    if (
+      this.visiblityState$.value ===
+      'none' /*|| this.visiblityState$$.value === 'none'*/
+    ) {
+      this.visiblityState$.next('expand')
+      // this.visiblityState$$.next('expand')
+    } else {
+      this.visiblityState$.next('none')
+      // this.visiblityState$$.next('none')
+    }
+    this.filterHidden = !this.filterHidden
+  }
 
   parallax() {
     //Loop over all the sections and set animations
@@ -108,6 +242,125 @@ export class HomeComponent implements OnInit {
     })
   }
 
+  textEffect() {
+    gsap.utils.toArray('.gs_reveal').forEach((elem: any) => {
+      this.hide(elem) // assure that the element is hidden when scrolled into view
+
+      ScrollTrigger.create({
+        trigger: elem,
+        onEnter: () => {
+          this.animateFrom(elem)
+        },
+        onEnterBack: () => {
+          this.animateFrom(elem, -1)
+        },
+        onLeave: () => {
+          this.hide(elem)
+        }, // assure that the element is hidden when scrolled into view
+      })
+    })
+  }
+
+  quoteEffect() {
+    const quote = gsap.timeline()
+
+    quote
+      .to('.quote-wrapper', {
+        transform: 'scaleX(1)',
+        duration: 1,
+        ease: 'power2.inOut',
+      })
+      .to('.quote', {
+        opacity: 1,
+        duration: 3,
+        ease: 'power2.inOut',
+      })
+  }
+
+  words = ['HELLO.', "I'M ANTHONY WONG.", 'A DEVELOPER.']
+  cursor: any
+  masterTl = gsap.timeline({ repeat: -1 }).pause()
+  boxTl = gsap.timeline()
+  nameEffect() {
+    this.cursor = gsap.to('.cursor', {
+      opacity: 0,
+      ease: 'power2.inOut',
+      repeat: -1,
+    })
+    this.boxTl
+      .to('.box', {
+        duration: 1,
+        width: '100%',
+        delay: 0,
+        ease: 'power4.inOut',
+      })
+      .to('.hello-abs', {
+        duration: 0.5,
+        y: 0,
+        ease: 'power3.out',
+      })
+      .to('.box', {
+        duration: 1,
+        height: '100%',
+        // ease: 'elastic.out',
+        onComplete: () => {
+          this.masterTl.play()
+        },
+      })
+      .to('.box', {
+        duration: 1,
+        autoAlpha: 0.7,
+        yoyo: true,
+        repeat: -1,
+        ease: "rough({ template: none.out, strength:  1, points: 20, taper: 'none', randomize: true, clamp: false})",
+      })
+    this.words.forEach(word => {
+      let tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1 })
+      tl.to('.typing', {
+        duration: 1,
+        text: word,
+        onComplete: () => {
+          if (word === 'A DEVELOPER.') {
+            this.masterTl.pause()
+            this.boxTl.pause()
+          }
+        },
+      })
+      this.masterTl.add(tl)
+    })
+  }
+
+  animateFrom(elem: any, _direction?: number) {
+    const direction = _direction || 1
+    var x = 0,
+      y = direction * 1000
+    if (elem.classList.contains('gs_reveal_fromLeft')) {
+      x = -100
+      y = 0
+    } else if (elem.classList.contains('gs_reveal_fromRight')) {
+      x = 100
+      y = 0
+    }
+    elem.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
+    elem.style.opacity = '0'
+    gsap.fromTo(
+      elem,
+      { x: x, y: y, autoAlpha: 0 },
+      {
+        duration: 1.25,
+        x: 0,
+        y: 0,
+        autoAlpha: 1,
+        ease: 'expo',
+        overwrite: 'auto',
+      }
+    )
+  }
+
+  hide(elem: any) {
+    gsap.set(elem, { autoAlpha: 0 })
+  }
+
   scroll(el: HTMLElement) {
     el.scrollIntoView({ block: 'start', behavior: 'smooth' })
   }
@@ -137,15 +390,19 @@ export class HomeComponent implements OnInit {
 
   onTouchEnd(event: any) {
     this.yAfter = event.changedTouches[0].clientY
-    this.debouncedOnScroll(this.yBefore - this.yAfter)
+    if (this.visiblityState$.value !== 'expand') {
+      this.debouncedOnScroll(this.yBefore - this.yAfter)
+    }
   }
 
   @HostListener('wheel', ['$event'])
   onScroll(event: any) {
-    console.log(event.type)
-    if (event.type === 'wheel') {
+    // console.log(event)
+    if (event.type === 'wheel' && !this.disableFullpage) {
       event.preventDefault()
-      this.debouncedOnScroll(event.deltaY)
+      if (this.visiblityState$.value !== 'expand' && this.loading === 'false') {
+        this.debouncedOnScroll(event.deltaY)
+      }
     }
   }
 }
